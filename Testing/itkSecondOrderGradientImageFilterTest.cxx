cbd836e5cf70363de5de534eec00ecad63192650
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-10-12 16:24:22 -0500

    Fill out content of the test.
    Test builds but does not pass.

--- Testing/itkSecondOrderGradientImageFilterTest.cxx
+++ Testing/itkSecondOrderGradientImageFilterTest.cxx
@@ -5,6 +5,7 @@ void RegisterTests()
   REGISTER_TEST(itkSecondOrderGradientImageFilterTest);
 }
 
+#include "itkGradientToMagnitudeImageFilter.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 
@@ -22,10 +23,54 @@ int itkSecondOrderGradientImageFilterTest(int argc, char *argv[])
 
   const unsigned int Dimension = 2;
   typedef float                              PixelType;
-  typedef itk::Image< PixelType, Dimension > InputImageType;
+  typedef itk::Image< PixelType, Dimension > ImageType;
 
-  typedef itk::ImageFileReader< InputImageType > ReaderType;
+  typedef itk::ImageFileReader< ImageType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::SecondOrderGradientImageFilter< ImageType, float > FilterType;
+  typedef FilterType::OutputImageType GradientImageType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+
+  typedef itk::ImageFileWriter< GradientImageType > GradientWriterType;
+  GradientWriterType::Pointer gradientWriter = GradientWriterType::New();
+  gradientWriter->SetInput( filter->GetOutput() );
+  std::string outputPrefix = argv[2];
+  gradientWriter->SetFileName( outputPrefix + "_Gradient.mha" );
+
+  try
+    {
+    gradientWriter->Update();
+    }
+  catch (itk::ExceptionObject& ex)
+    {
+    std::cerr << "Exception caught!" << std::endl;
+    std::cerr << ex << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef itk::GradientToMagnitudeImageFilter< GradientImageType, ImageType >
+    GradientMagnitudeFilterType;
+  GradientMagnitudeFilterType::Pointer gradientMagnitude = GradientMagnitudeFilterType::New();
+  gradientMagnitude->SetInput( filter->GetOutput() );
+
+  typedef itk::ImageFileWriter< ImageType > GradientMagnitudeWriterType;
+  GradientMagnitudeWriterType::Pointer gradientMagnitudeWriter = GradientMagnitudeWriterType::New();
+  gradientMagnitudeWriter->SetInput( gradientMagnitude->GetOutput() );
+  gradientMagnitudeWriter->SetFileName( outputPrefix + "_GradientMagnitude.mha" );
+
+  try
+    {
+    gradientMagnitudeWriter->Update();
+    }
+  catch (itk::ExceptionObject& ex)
+    {
+    std::cerr << "Exception caught!" << std::endl;
+    std::cerr << ex << std::endl;
+    return EXIT_FAILURE;
+    }
 
   return EXIT_SUCCESS;
 }
