238ef8dbb220d23b075e8c3694b9d05cf1fb25a7
Matt McCormick <matt.mccormick@kitware.com>
2019-02-11 20:01:00 -0500

    BUG: Python package dependss on itk>=5.0rc01

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     keywords='ITK Higher-order Derivative Gradient',
     url=r'https://github.com/InsightSoftwareConsortium/ITKHigherOrderAccurateGradient',
     install_requires=[
-        r'itk>=5'
+        r'itk>=5.0rc01'
     ]
     )
