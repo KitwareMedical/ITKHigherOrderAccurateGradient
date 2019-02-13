861b126fc2f880223c94849fd647e282f8d4b48e
Matt McCormick <matt.mccormick@kitware.com>
2019-02-12 23:33:50 -0500

    ENH: Bump Python package version to 1.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-higherorderaccurategradient',
-    version='1.0.1',
+    version='1.0.2',
     author='Matthew M. McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
